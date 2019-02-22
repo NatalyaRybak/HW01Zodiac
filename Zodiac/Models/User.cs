8a95799c8c07e311df9c46ae24c93b351f781f62
Nataly Rybak <natalya.rybak@gmail.com>
2019-02-22 23:58:21 +0200

    First commit

---
+++ Zodiac/Models/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Zodiac.Models
+{
+    internal class User
+    {
+        private DateTime _birthDate;
+        internal string Age { get; private set; }
+        internal string WesternSign { get; private set; }
+        internal string ChineseSign { get; private set; }
+        internal bool Executable { get; private set; }
+        private readonly string[] _westernSigns = { "Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius" };
+        private readonly string[] _chineseSigns = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
+
+        public bool Congrats => DateTime.Today.Month == _birthDate.Month && DateTime.Today.Day == _birthDate.Day;
+
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set
+            {
+                _birthDate = value;
+                var age = (DateTime.Today.Year - value.Year) - (DateTime.Today.DayOfYear >= _birthDate.DayOfYear ? 0 : 1);
+                var diff = DateTime.Today - value;
+                Executable = diff.Days >= 0 && age <= 135;
+
+                if ( Executable )
+                {
+                    Age = age > 0 ? age + " year(s)" : diff.Days + " day(s)";
+                    ChineseSign = _chineseSigns[(_birthDate.Year + 8) % 12];
+                    var bmonth = _birthDate.Month;
+                    var bday = _birthDate.Day;
+                    switch (bmonth)
+                    {
+                        case 1:
+                            WesternSign = bday >= 20 ? _westernSigns[1] : _westernSigns[0];
+                            break;
+                        case 2:
+                            WesternSign = bday >= 19 ? _westernSigns[2] : _westernSigns[1];
+                            break;
+                        case 3:
+                            WesternSign = bday >= 21 ? _westernSigns[3] : _westernSigns[2];
+                            break;
+                        case 4:
+                            WesternSign = bday >= 20 ? _westernSigns[4] : _westernSigns[3];
+                            break;
+                        case 5:
+                            WesternSign = bday >= 21 ? _westernSigns[5] : _westernSigns[4];
+                            break;
+                        case 6:
+                            WesternSign = bday >= 21 ? _westernSigns[6] : _westernSigns[5];
+                            break;
+                        case 7:
+                            WesternSign = bday >= 23 ? _westernSigns[7] : _westernSigns[6];
+                            break;
+                        case 8:
+                            WesternSign = bday >= 23 ? _westernSigns[8] : _westernSigns[7];
+                            break;
+                        case 9:
+                            WesternSign = bday >= 23 ? _westernSigns[9] : _westernSigns[8];
+                            break;
+                        case 10:
+                            WesternSign = bday >= 23 ? _westernSigns[10] : _westernSigns[9];
+                            break;
+                        case 11:
+                            WesternSign = bday >= 22 ? _westernSigns[11] : _westernSigns[10];
+                            break;
+                        case 12:
+                            WesternSign = bday >= 22 ? _westernSigns[0] : _westernSigns[11];
+                            break;
+
+                    }
+
+                }
+            }
+        }
+
+    }
+}
---
+++ Zodiac/ViewModels/DateViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using KMA.ProgrammingInCSharp2019.Practice3.LoginControlMVVM.Properties;
+using Zodiac.Models;
+using Zodiac.Tools;
+
+namespace Zodiac.ViewModels
+{
+    internal class DateViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+        private readonly User _user = new User();
+        private string _visibilityText = "Hidden";
+
+        #region Commands
+        private RelayCommand<object> _findSignCommand;
+        #endregion
+        #endregion
+
+        #region Properties
+        public DateTime BirthDate
+        {
+            get { return _user.BirthDate; }
+            set {
+                _user.BirthDate = value;
+
+                if ( _user.Congrats )
+                {
+                    MessageBox.Show("Happy birthday!!!");
+
+                }
+                
+                OnPropertyChanged();
+
+            }
+        }
+
+        #region Commands
+
+        public RelayCommand<object> FindSignCommand
+        {
+            get
+
+           {
+                return _findSignCommand ?? (_findSignCommand = new RelayCommand<object>(
+                           o =>
+                           {
+                               VisibilityText = "Visible";
+
+                           }, o => CanExecuteCommand()));
+            }
+        }
+        #endregion
+        #endregion
+
+        public string VisibilityText
+        {
+            get { return _visibilityText; }
+            set
+            {
+                _visibilityText = value;
+                OnPropertyChanged ();
+            }
+        }
+
+        public bool CanExecuteCommand ()
+        {
+            
+            return !((_birthDate > DateTime.Now) || (Age() > 135));
+        }
+
+        private int Age()
+        {
+            if (_birthDate.Month > DateTime.Now.Month ||(_birthDate.Month == DateTime.Now.Month && _birthDate.Day > DateTime.Now.Day))
+            {
+                return DateTime.Now.Year - _birthDate.Year - 1;
+
+            }
+            else if (_birthDate.Month<DateTime.Now.Month ||(_birthDate.Month == DateTime.Now.Month && _birthDate.Day <= DateTime.Now.Day))
+            {
+             return  DateTime.Now.Year - _birthDate.Year;
+            }
+
+            return 0;
+        }
+
+        private void
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+
+}
